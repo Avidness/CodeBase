d0abca7259b1979e649c700392ebc552d4755c60
Alan Ness <userbon@scimitar.local>
2017-12-17 17:24:43 -0600

    Connect dotnet with react

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,32 @@
-import webpack from 'webpack';
-import path from 'path';
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const bundleOutputDir = './wwwroot/dist';
 
-export default {
-  debug: true,
-  devtool: 'inline-source-map',
-  noInfo: false,
-  entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
-    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    path.resolve(__dirname, 'src/index')
-  ],
-  target: 'web',
-  output: {
-    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'src')
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module: {
-    loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-    ]
-  }
+module.exports = (env) => {
+  const isDevBuild = !(env && env.prod);
+  return [{
+    stats: { modules: false },
+    entry: { index: './ClientApp/index.js' },
+    resolve: { extensions: ['.js', '.jsx'] },
+    output: {
+      path: path.join(__dirname, bundleOutputDir),
+      filename: '[name].js',
+      publicPath: 'dist/'
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ],
+    module: {
+      loaders: [
+        {test: /\.js$/, include: path.join(__dirname, 'ClientApp'), loaders: ['babel-loader']},
+        {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']},
+        {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+        {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000'},
+        {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+        {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
+      ]
+    }
+  }];
 };
