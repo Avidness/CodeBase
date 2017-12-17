c96d2fa0f1ca4e49ac22082141ab368bf956186f
Alan Ness <userbon@scimitar.local>
2017-12-17 17:28:41 -0600

    Fix spacing

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,28 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const bundleOutputDir = './wwwroot/dist';
 
 module.exports = (env) => {
-  const isDevBuild = !(env && env.prod);
-  return [{
-    stats: { modules: false },
-    entry: { index: './ClientApp/index.js' },
-    resolve: { extensions: ['.js', '.jsx'] },
-    output: {
-      path: path.join(__dirname, bundleOutputDir),
-      filename: '[name].js',
-      publicPath: 'dist/'
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoEmitOnErrorsPlugin()
-    ],
-    module: {
-      loaders: [
-        {test: /\.js$/, include: path.join(__dirname, 'ClientApp'), loaders: ['babel-loader']},
-        {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']},
-        {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
-        {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000'},
-        {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
-        {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
-      ]
-    }
-  }];
+    const isDevBuild = !(env && env.prod);
+    return [{
+        stats: { modules: false },
+        entry: { index: './ClientApp/index.js' },
+        resolve: { extensions: ['.js', '.jsx'] },
+        output: {
+            path: path.join(__dirname, bundleOutputDir),
+            filename: '[name].js',
+            publicPath: 'dist/'
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoEmitOnErrorsPlugin()
+        ],
+        module: {
+        loaders: [
+            {test: /\.js$/, include: path.join(__dirname, 'ClientApp'), loaders: ['babel-loader']},
+            {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+            {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
+        ]}
+    }];
 };
