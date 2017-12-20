757857cf285b71e1f64966a59525ad0d31db629c
Alan Ness <userbon@scimitar.local>
2017-12-19 22:01:32 -0600

    Remove unused proptype import

--- ClientApp/components/App.js
+++ ClientApp/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './common/Header';
 import Main from './Main';
-import PropTypes from 'prop-types';
 
 const App = () => (
     <div className="container-fluid">
